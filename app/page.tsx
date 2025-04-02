ea29d2bcd222d105ee220e2d7075f6b1f2d42ff7
LosOleksandr <losalexander2003@gmail.com>
2025-04-02 15:37:53 +0300

    feat: implement theme logic, color pallete and font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import ThemeToggler from '@/components/theme-toggler';
+
 export default function Home() {
   return (
     <div className="grid h-screen w-screen place-content-center">
       Todo list Test App
+      <div className="bg-success max-w-xs text-lg">Todo Task</div>
+      <div className="bg-danger max-w-xs text-lg">Todo Task</div>
+      <div className="bg-muted max-w-xs text-lg">Todo Task</div>
+      <div className="bg-accent max-w-xs text-lg">Todo Task</div>
+      <ThemeToggler />
     </div>
   );
 }
